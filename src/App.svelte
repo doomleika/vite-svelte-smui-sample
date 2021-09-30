3a5036287d84572a6eb7fea60b8c8ebd1f7937e3
doomleika <doomleika@gmail.com>
2021-09-30 17:52:09 +0800

    use app as base

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
 </main>
 
 <style>
+  @import 'svelte-material-ui/bare.css';
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
